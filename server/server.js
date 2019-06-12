dc97c10b62a9b61ddbbb056f72aba4e4970c321d
Mandeep Kumar <mandeepkumar8267@gmail.com>
2019-06-12 19:03:18 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path=require('path');
+const express=require('express');
+const publicPath=path.join(__dirname, '../public');
+
+// console.log(__dirname+'/../public');//
+// console.log(publicPath);
+const port=process.env.PORT || 3000;
+var app=express();
+
+app.use(express.static(publicPath));
+
+app.get('/server',(req,res)=>{
+  res.send("Hello this express!");
+});
+
+
+app.listen(port,()=>{
+  console.log(`Server is up on ${port}`);
+});
